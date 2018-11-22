98510634dcb2486c883eb7c9cf6c5ab2bdd45ee3
XeniaOlshanskaya <ks14012000@gmail.com>
2018-11-22 22:13:07 +0700

    process

--- project-5.py
+++ project-5.py
@@ -1,10 +1,19 @@
-def women_cals(y, z, w):
+def women_cals(y, z, w, e):
     result = (10 * z + 6.25 * w - 5 * y - 161)
-    return str(result)
+    total_result(result, e)
 
-def men_cals(y, z, w):
+def men_cals(y, z, w, e):
     result = (10 * z + 6.25 * w - 5 * y + 5)
-    return str(result)
+    total_result(result, e)
+
+def total_result(result, e):
+    if e == 1:
+        total_result = result * 1.6375
+    elif e == 2:
+        total_result = result * 1.375
+    else:
+        total_result = result * 1.2
+    return total_result
 
 
 def main():
@@ -17,11 +26,14 @@ def main():
         weight = float(input('Введите свой вес (в киллограммах): '))
         height = float(input('Введите свой рост (в сантиметрах): '))
         print('Как часто вы занимаетесь спортом (физкультурой)?')
-        exercise = input('Введите: 1 - если часто; 2 - редко; 3 - вообще не занимаюсь: ')
-        if gender == "ж" and exercise:
-            result = women_cals(age, weight, height)
+        exercise = int(input('Введите: 1 - если часто; 2 - редко; 3 - вообще не занимаюсь: '))
+        while exercise != 1 and exercise != 2 and exercise != 3:
+            exercise = int(input('Вы ввели неверное значение. Введите: 1 - если часто; 2 - редко;'
+                             ' 3 - вообще не занимаюсь: '))
+        if gender == "ж":
+            result = women_cals(age, weight, height, exercise)
         elif gender == "м":
-            result = men_cals(age, weight, height)
+            result = men_cals(age, weight, height, exercise)
 
     except ValueError:
         print('Вы ввели неверное значение. Перезапустите программу для повторного рассчета.')
